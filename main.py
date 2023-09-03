a1a4b8f9984d0ea1a3db484e3a0735aa63ca323d
shibbir <shibbir63@yahoo.com>
2023-09-03 19:56:14 +0100

    Add footer.

--- main.py
+++ main.py
@@ -2,49 +2,32 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
-# print(type(pdf))
-
-# pdf.add_page()
-# pdf.set_font(family="Times", style="B", size=12)
-# pdf.cell(w=0, h=12, txt="Hello there", align="L", ln=1, border=1)
-# pdf.set_font(family="Times", style="B", size=10)
-# pdf.cell(w=0, h=12, txt="H|i there", align="L", ln=1, border=1)
-#
-# pdf.add_page()
-# pdf.set_font(family="Times", style="B", size=12)
-# pdf.cell(w=0, h=12, txt="Hello there", align="L", ln=1, border=1)
-# pdf.set_font(family="Times", style="B", size=10)
-# pdf.cell(w=0, h=12, txt="H|i there", align="L", ln=1, border=1)
-#
-# pdf.output("output.pdf")
-
-len(df["Pages"])
-
-
-# for item in df["Topic"]:
-#     print(item)
-#     print(df["Pages"][0])
-#     for pages in len(df["Pages"][0]):
-#     pdf.add_page()
-#     pdf.set_font(family="Times", style="B", size=12)
-#     pdf.cell(w=0, h=12, txt=item, align="L", ln=1, border=1)
-#     pdf.set_font(family="Times", style="B", size=10)
-#     # pdf.cell(w=0, h=12, txt="H|i there", align="L", ln=1, border=1)
 
 for index, row in df.iterrows():
-
     pdf.add_page()
+    # Set the header
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(0, 0, 254)
+    pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
+
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
 
-
+        # Set the footer
+        pdf.ln(275)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
